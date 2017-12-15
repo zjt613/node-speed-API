60c0d07ea36c18b25f5ad00eba3b0cf957b33ec7
aikefang <github@aikefang.com>
2017-12-15 13:55:44 +0800

    Update keepFiles.js
    优化保存文件至结构化文件夹

--- plugins/keepFiles.js
+++ plugins/keepFiles.js
@@ -1,4 +1,51 @@
 var uuid = require('node-uuid');
+var path = require('path');
+var fs = global.custom.fs;
+//创建文件夹
+function mkdir(pos, dirArray,_callback){
+    var len = dirArray.length;
+    console.log(len);
+    if( pos >= len || pos > 10){
+        _callback();
+        return;
+    }
+    var currentDir = '';
+    for(var i= 0; i <=pos; i++){
+        if(i!=0)currentDir+='/';
+        currentDir += dirArray[i];
+    }
+    fs.exists(currentDir,function(exists){
+        if(!exists){
+            fs.mkdir(currentDir,function(err){
+                if(err){
+                    console.log('创建文件夹出错！');
+                }else{
+                    console.log(currentDir+'文件夹-创建成功！');
+                    mkdir(pos+1,dirArray,_callback);
+                }
+            });
+        }else{
+            console.log(currentDir+'文件夹-已存在！');
+            mkdir(pos+1,dirArray,_callback);
+        }
+    });
+}
+
+//创建目录结构
+function mkdirs(dirpath,_callback) {
+    var dirArray = dirpath.split('/');
+    fs.exists( dirpath ,function(exists){
+        if(!exists){
+            mkdir(0, dirArray,function(){
+                console.log('文件夹创建完毕!准备写入文件!');
+                _callback();
+            });
+        }else{
+            console.log('文件夹已经存在!准备写入文件!');
+            _callback();
+        }
+    });
+}
 /**
  *
  * @param options.fileParam {String} (必填) 文件信息
@@ -10,9 +57,12 @@ var uuid = require('node-uuid');
  */
 var keepFiles = function (options,success,error) {
     var that = this;
+    //格式化日期
+    var date = new Date().Format('yyyy-MM-dd');
     // 兼容options.directory非必填
-    options.directory = 'files/'+(!!options.directory?options.directory:'default/');
-    global.custom.fs.mkdir(options.directory,function () {
+    options.directory = 'files/'+(!!options.directory?options.directory+date+'/':'default/'+date+'/');
+
+    mkdirs(options.directory,function () {
         global.custom.fs.readFile( options.cacheFile, function (err, data) {
             // 获取后缀
             var suffix = '.'+options.fileParam.originalname.split('.')[(options.fileParam.originalname.split('.').length-1)];
@@ -29,6 +79,7 @@ var keepFiles = function (options,success,error) {
             if(options.directory[options.directory.length-1] != '/'){
                 options.directory += '/'
             }
+
             global.custom.fs.writeFile(options.directory+fileName, data, function (err) {
                 //删除缓存文件 兼容options.cacheFile非必填
                 global.custom.plugins.deleteCacheFile(options.cacheFile || options.fileParam.path);
